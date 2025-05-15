419aea70b877f0c19be15c4476a8bb21684544e3
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-05-15 13:56:17 +0900

    封装数据库连接和关闭资源的操作

---
+++ src/test/java/com/atguigu/jdbc/d_util/JDBCUtils.java
@@ -0,0 +1,69 @@
+package com.atguigu.jdbc.d_util;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+/**
+ * ClassName: JDBCUtils
+ * Package: com.atguigu.jdbc.d_util
+ * Description:
+ * 操作数据库的工具类
+ *
+ * @Author ljy
+ * @Create 2025. 5. 15. 오후 1:48
+ * @Version 1.0
+ */
+public class JDBCUtils {
+
+    /**
+     * 获取数据库的连接
+     *
+     * @return
+     * @throws Exception
+     */
+    public static Connection getConnection() throws Exception {
+        // 1. 读取配置文件中的4个基本信息
+        InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("jdbc.properties");
+
+        Properties props = new Properties();
+        props.load(is);
+
+        String user = props.getProperty("user");
+        String password = props.getProperty("password");
+        String url = props.getProperty("url");
+        String driverClass = props.getProperty("driverClass");
+
+        // 2. 加载驱动
+        Class.forName(driverClass);
+
+        // 3. 获取连接
+        Connection conn = DriverManager.getConnection(url, user, password);
+        return conn;
+    }
+
+    /**
+     * 关闭资源的操作
+     *
+     * @param conn
+     * @param ps
+     */
+    public void closeResource(Connection conn, Statement ps) {
+        try {
+            if (ps != null)
+                ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
