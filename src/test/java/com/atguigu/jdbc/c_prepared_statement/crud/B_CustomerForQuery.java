ff2b01f41f4699d39dda5749c0eca11fe64667ae
liangjinyong <liangjy881@gmail.com>
2025-05-15 22:12:44 +0900

    针对customers表的通用的查询操作

--- src/test/java/com/atguigu/jdbc/c_prepared_statement/crud/B_CustomerForQuery.java
+++ src/test/java/com/atguigu/jdbc/c_prepared_statement/crud/B_CustomerForQuery.java
@@ -4,10 +4,8 @@ import com.atguigu.jdbc.d_util.JDBCUtils;
 import com.atguigu.jdbc.e_bean.Customer;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.lang.reflect.Field;
+import java.sql.*;
 
 /**
  * ClassName: CustomerForQuery
@@ -59,5 +57,61 @@ public class B_CustomerForQuery {
             // 关闭资源
             JDBCUtils.closeResource(conn, ps, resultSet);
         }
+    }
+
+    /**
+     * 针对于customers表的通用的查询操作
+     */
+    public Customer queryForCustomers(String sql, Object... args) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = JDBCUtils.getConnection();
+
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            rs = ps.executeQuery();
+            // 获取结果集的元数据: ResultSetMetaData
+            ResultSetMetaData rsmd = rs.getMetaData();
+            // 通过ResultSetMetaData获取结果集中的列数
+            int columnCount = rsmd.getColumnCount();
+            if (rs.next()) {
+                Customer cust = new Customer();
+                // 处理结果集一行数据中的每一个列
+                for (int i = 0; i < columnCount; i++) {
+                    // 获取列值
+                    Object columnValue = rs.getObject(i + 1);
+
+                    // 获取每个列的列名
+                    String columnName = rsmd.getColumnName(i + 1);
+
+                    // 给cust对象指定的columnName属性，赋值为columnValue，通过反射
+                    Field field = Customer.class.getDeclaredField(columnName);
+                    field.setAccessible(true);
+                    field.set(cust, columnValue);
+                }
+                return cust;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, ps, rs);
+        }
+        return null;
+    }
+
+    @Test
+    public void testQueryForCustomers() {
+        String sql = "select id, name, birth, email from customers where id = ?";
+        Customer customer = queryForCustomers(sql, 13);
+        System.out.println(customer);
+
+        sql = "select name, email from customers where name = ?";
+        Customer customer1 = queryForCustomers(sql, "周杰伦");
+        System.out.println(customer1);
     }
 }
