977656e3767a63cc38f407f45accfb2bae2d2ce4
liangjinyong <liangjy881@gmail.com>
2025-05-16 16:38:18 +0900

    从数据表中读取Blob类型的数据

--- src/test/java/com/atguigu/jdbc/g_blob/BlobTest.java
+++ src/test/java/com/atguigu/jdbc/g_blob/BlobTest.java
@@ -1,12 +1,11 @@
 package com.atguigu.jdbc.g_blob;
 
 import com.atguigu.jdbc.d_util.JDBCUtils;
+import com.atguigu.jdbc.e_bean.Customer;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.io.*;
+import java.sql.*;
 
 /**
  * ClassName: BlobTest
@@ -37,5 +36,67 @@ public class BlobTest {
         ps.execute();
 
         JDBCUtils.closeResource(conn, ps);
+    }
+
+    // 查询数据表customers中Blob类型的数据
+    @Test
+    public void testQuery() {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        InputStream is = null;
+        FileOutputStream fos = null;
+        ResultSet rs = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            String sql = "select id, name, email, birth, photo from customers where id = ?";
+            ps = conn.prepareStatement(sql);
+
+            ps.setInt(1, 21);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                // 方式1:
+                /*
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                String email = rs.getString(3);
+                Date birth = rs.getDate(4);
+                 */
+                // 方式2:
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                Date birth = rs.getDate("birth");
+
+                Customer cust = new Customer(id, name, email, birth);
+                System.out.println(cust);
+
+                // 将Blob类型的字段下载下来，以文件的方式保存在本地
+                Blob photo = rs.getBlob("photo");
+                is = photo.getBinaryStream();
+
+                fos = new FileOutputStream("shk2.png");
+                byte[] buffer = new byte[1024];
+                int len;
+                while ((len = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, len);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null)
+                    is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (fos != null)
+                    fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            JDBCUtils.closeResource(conn, ps, rs);
+        }
     }
 }
