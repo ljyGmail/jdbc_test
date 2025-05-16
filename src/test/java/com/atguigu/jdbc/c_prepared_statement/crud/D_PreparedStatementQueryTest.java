e2a27e6184d61f58bcfd95256043c3af4b89a229
liangjinyong <liangjy881@gmail.com>
2025-05-16 10:50:01 +0900

    PreparedStatement针对不同表的通用查询操作1

---
+++ src/test/java/com/atguigu/jdbc/c_prepared_statement/crud/D_PreparedStatementQueryTest.java
@@ -0,0 +1,79 @@
+package com.atguigu.jdbc.c_prepared_statement.crud;
+
+import com.atguigu.jdbc.d_util.JDBCUtils;
+import com.atguigu.jdbc.e_bean.Customer;
+import com.atguigu.jdbc.e_bean.Order;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+/**
+ * ClassName: D_PreparedStatementQueryTest
+ * Package: com.atguigu.jdbc.c_prepared_statement.crud
+ * Description:
+ * 使用PreparedStatement实现针对于不同表的通用的查询操作
+ *
+ * @Author: ljy
+ * @Create: 2025. 5. 16. 오전 10:40
+ * @Version 1.0
+ */
+public class D_PreparedStatementQueryTest {
+
+    public <T> T getInstance(Class<T> clazz, String sql, Object... args) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = JDBCUtils.getConnection();
+
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            rs = ps.executeQuery();
+            // 获取结果集的元数据: ResultSetMetaData
+            ResultSetMetaData rsmd = rs.getMetaData();
+            // 通过ResultSetMetaData获取结果集中的列数
+            int columnCount = rsmd.getColumnCount();
+            if (rs.next()) {
+                T t = clazz.newInstance();
+                // 处理结果集一行数据中的每一个列
+                for (int i = 0; i < columnCount; i++) {
+                    // 获取列值
+                    Object columnValue = rs.getObject(i + 1);
+
+                    // 获取每个列的列名
+                    // String columnName = rsmd.getColumnName(i + 1);
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    // 给cust对象指定的columnName属性，赋值为columnValue，通过反射
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columnValue);
+                }
+                return t;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, ps, rs);
+        }
+        return null;
+    }
+
+    @Test
+    public void testGetInstance() {
+        String sql = "select id, name, email from customers where id = ?";
+        Customer customer = getInstance(Customer.class, sql, 12);
+        System.out.println(customer);
+
+        String sql1 = "select order_id orderId, order_name orderName from `order` where order_id = ?";
+        Order order = getInstance(Order.class, sql1, 1);
+        System.out.println(order);
+    }
+}
