fddc6e0d71c736ec697fbf276dc94af51106c37d
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-05-19 14:31:06 +0900

    使用QueryRunner查询表中一条或多条记录的操作

--- src/test/java/com/atguigu/jdbc/m_dbutils/QueryRunnerTest.java
+++ src/test/java/com/atguigu/jdbc/m_dbutils/QueryRunnerTest.java
@@ -2,11 +2,18 @@ package com.atguigu.jdbc.m_dbutils;
 
 import com.atguigu.jdbc.d_util.JDBCUtils;
 import com.atguigu.jdbc.d_util.JDBCUtilsWithDataSource;
+import com.atguigu.jdbc.e_bean.Customer;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * ClassName: QueryRunnerTest
@@ -31,6 +38,90 @@ public class QueryRunnerTest {
             String sql = "insert into customers(name, email, birth) values(?, ?, ?)";
             int insertCount = runner.update(conn, sql, "周阳", "zhouyang@126.com", "1973-05-19");
             System.out.println("添加了" + insertCount + "条记录");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    // 测试查询
+
+    /*
+     BeanHandler: 是ResultSetHandler接口的实现类，用于封装表中的一条记录。
+     */
+    @Test
+    public void testQuery1() {
+        Connection conn = null;
+        try {
+            QueryRunner runner = new QueryRunner();
+            conn = JDBCUtilsWithDataSource.getConnection3();
+            String sql = "select id, name, email, birth from customers where id = ?";
+            BeanHandler<Customer> handler = new BeanHandler<>(Customer.class);
+            Customer customer = runner.query(conn, sql, handler, 21);
+            System.out.println(customer);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    /*
+     BeanListHandler: 是ResultSetHandler接口的实现类，用于封装表中的多条记录构成的集合。
+     */
+    @Test
+    public void testQuery2() {
+        Connection conn = null;
+        try {
+            QueryRunner runner = new QueryRunner();
+            conn = JDBCUtilsWithDataSource.getConnection3();
+            String sql = "select id, name, email, birth from customers where id < ?";
+            BeanListHandler<Customer> handler = new BeanListHandler<>(Customer.class);
+            List<Customer> list = runner.query(conn, sql, handler, 21);
+            list.forEach(System.out::println);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    /*
+     MapHandler: 是ResultSetHandler接口的实现类，对应表中的一条记录。
+     将字段及相应字段的值作为map中的key和value。
+     */
+    @Test
+    public void testQuery3() {
+        Connection conn = null;
+        try {
+            QueryRunner runner = new QueryRunner();
+            conn = JDBCUtilsWithDataSource.getConnection3();
+            String sql = "select id, name, email, birth from customers where id = ?";
+            MapHandler handler = new MapHandler();
+            Map<String, Object> map = runner.query(conn, sql, handler, 21);
+            System.out.println(map);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    /*
+     MapListHandler: 是ResultSetHandler接口的实现类，对应表中的多条记录。
+     将字段及相应字段的值作为map中的key和value。
+     */
+    @Test
+    public void testQuery4() {
+        Connection conn = null;
+        try {
+            QueryRunner runner = new QueryRunner();
+            conn = JDBCUtilsWithDataSource.getConnection3();
+            String sql = "select id, name, email, birth from customers where id < ?";
+            MapListHandler handler = new MapListHandler();
+            List<Map<String, Object>> list = runner.query(conn, sql, handler, 20);
+            list.forEach(System.out::println);
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
